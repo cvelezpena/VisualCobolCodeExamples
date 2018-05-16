2f49a144a5ec6e34aa87d0dc9a5f93271be78a37
Peter Liljenberg <peli@bluegarden.com>
2018-05-16 18:32:05 +0200

    Add RunUnit example

---
+++ RunUnitProgram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Demo.Cobol;
+using MicroFocus.COBOL.RuntimeServices;
+
+namespace RunUnitProgram
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            // Create a new isolated COBOL runtime
+            using (var runUnit = new RunUnit())
+            {
+                // Instantiate the COBOL class to use in the runtime
+                var logger = new CobolLogger
+                {
+                    UserId = "testuser"
+                };
+
+                // Add it to the runtime
+                runUnit.Add(logger);
+
+                // All calls to methods in the COBOL class now executes in this runtime
+                logger.Warning("Warning from .NET");
+                logger.Error(string.Format("Test error {0}", logger.GetGuid()));
+
+                logger.UserId = "anotheruser";
+
+                logger.Warning("Another warning");
+                logger.Error("And another error");
+
+                Console.ReadKey();
+
+                // Clean up runtime resources
+                runUnit.StopRun();
+            }
+        }
+    }
+}
